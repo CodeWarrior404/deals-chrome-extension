2399e27f27d6592d9858ccff072048fa104bd01b
Pradeep James <pjm123mail-github@yahoo.co.in>
2017-09-03 16:44:03 -0500

    Added code to retrieve key words from page text

--- popup.js
+++ popup.js
@@ -23,9 +23,29 @@ function updateDisplay(displayValue) {
 
 function proccessPageText(pageTxt) {
     updateDisplay('Checking for deals...');
-    $.ajax({
-        url: 'https://backflipp.wishabi.com/flipp/items/search?locale=en-us&postal_code=75056&q=printer'
-    }).then(function(data) {
-        $('#parsedTextDiv').text(data);
-    });
+    updateDisplay(createListOfKeywordsFromPageTxt(pageTxt));
+    // $.ajax({
+    //     url: 'https://backflipp.wishabi.com/flipp/items/search?locale=en-us&postal_code=75056&q=printer'
+    // }).then(function(data) {
+    //     $('#parsedTextDiv').text(data);
+    // });
+}
+
+function createListOfKeywordsFromPageTxt(pageTxt) {
+    // List of words to ignore
+    var ignoreList = { add: true, and: true, information: true, large: true, nutrition: true, servings: true,
+        small: true, sprinkle: true, stir: true, the: true, them: true, total: true, 'with': true, knead: true, each: true,
+        roll: true };
+
+    pageTxt = pageTxt.replace(/[&\/\\#,+()$~%.'":*?<>{}_@;\-\r\n]/g, ' ');
+    var wordArray = pageTxt.split(' ');
+    var wordMap = {};
+
+    for (var i = 0; i < wordArray.length; i++) {
+        var word = wordArray[i];
+        if (word.length > 2 && !wordMap[word.toLowerCase()] && !ignoreList[word.toLowerCase()]) {
+            wordMap[word.toLowerCase()] = true;
+        }
+    }
+    return Object.keys(wordMap);
 }
